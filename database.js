d0d47ec530ee43df24c117eba2b8451677532b7c
bradyAdger12 <brady@traildock.com>
2022-06-02 16:06:01 -0600

    track db file

---
+++ database.js
@@ -0,0 +1,21 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    useUTC: true, // --> Add this line. for reading from database
+},
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+module.exports = sequelize
