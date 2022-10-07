ba94adeab32b065d7840d90a1a79875e109c8926
bradyAdger12 <brady@traildock.com>
2022-10-07 14:58:13 -0600

    remove body parser

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV == 'development') {
 }
 const express = require('express')
 const app = express()
-var bodyParser = require('body-parser');
 var cors = require('cors')
 const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
@@ -43,8 +42,8 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use('/users', require('./controllers/users_controller'))
 app.use('/workouts', require('./controllers/workouts_controller'))
 app.use('/strava', require('./controllers/strava_controller'))
