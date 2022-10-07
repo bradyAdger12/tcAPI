6ab7304c08314e0115dfe277f23365b36e3c2d7d
bradyAdger12 <brady@traildock.com>
2022-10-07 12:37:50 -0600

    tweaks to db connection

--- database.js
+++ database.js
@@ -3,10 +3,11 @@ var sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, proces
   host: process.env.PGHOST,
   dialect: 'postgres',
   logging: false,
-  ssl: true,
   dialectOptions: {
     useUTC: true, // --> Add this line. for reading from database
-},
+    ssl: true,
+    native: true
+  },
 });
 
 sequelize
