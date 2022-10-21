dcfb06bc7703e6cca93c39c1a6aa3f0075627314
bradyAdger12 <brady@traildock.com>
2022-10-21 08:44:43 -0600

    add log

--- tools/summary.js
+++ tools/summary.js
@@ -2,6 +2,8 @@ const Workout = require('../models/workout.js')
 const { Op } = require('sequelize')
 const moment = require('moment')
 getSummary = async function (actor, startDate, endDate) {
+  console.log(startDate.toISOString())
+  console.log(endDate.toISOString())
   let workouts = await Workout.findAll({
     order: [
       ['started_at', 'DESC']],
